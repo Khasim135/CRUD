1da4a733db6a4ac57090f2cb3a87874db5969c09
Khasimpeera <124671695+Khasim135@users.noreply.github.com>
2023-02-16 00:14:52 +0530

    Rules.js
    these are the rule must follow to run this project

---
+++ Rules.js
@@ -0,0 +1,24 @@
+to run this project we need install some packages in react ja 
+to install packages go to new terminal and 
+
+this packages are most impoortant to run the project 
+
+1) json-server to run json file to make url connection we need json-server 
+ 
+ type in terminal  ( "npm install json-server ") 
+ after installation you can see in package.json  ( "json-server": "^0.17.1", )
+ 
+2)after installate type this to run json files   (" npx json-server Backend/database.json --watch port 5000 " )
+than it will opened in new url than we use that url by //axios.get("http://localhost:3000/content")
+  
+3) axios to get data from url links it make link between database.json and files 
+it contains some pre defined methods 
+( "npm install axios" ) type and check in package.json file you can see this   ( "axios": "^1.3.2", ) 
+
+4) react-router-dom   install this package to make links router connections 
+most important package in react-js 
+type in terminal ( " npm install react-router-dom ")
+  than you can check in package.json file ( "react-router-dom": "^6.8.1", )
+  
+  
+ 
